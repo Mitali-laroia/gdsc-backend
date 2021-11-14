8304030ce7597aa37986f5846eda38599f898ccc
Mitali-laroia <04042001mitali@gmail.com>
2021-11-14 18:50:28 +0530

    initial push

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/",function(req,res){
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
+    
+    var result = (weight / (height*height))*703;
+
+    if(result<25)
+        res.send("Result : " + result + "good bmi");
+    else 
+    res.send("Result : " + result + "not healthy");
+    
+})
+
+app.listen(3000, function(){
+    console.log("Server up and running at port 3000");
+})
